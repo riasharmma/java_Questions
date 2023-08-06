ba19e73f9fb50ba4f5022f5598bee4fd08e233cf
Riya Sharma <riyasharmma020103@gmail.com>
2023-08-06 11:55:49 +0530

    second commit

--- Assignment8.java
+++ Assignment8.java
@@ -4,44 +4,7 @@ elements such that their sum is closest to zero.*/
 package coding;
 import java.util.*;
 public class Assignment8 {
-
-    static void minAbsSumPair(int arr[], int arr_size)
-    {
-      int inv_count = 0;
-      int l, r, min_sum, sum, min_l, min_r;
-      
-      /* Array should have at least two elements*/
-      if(arr_size < 2)
-      {
-        System.out.println("Invalid Input");
-        return;
-      }
-      
-      /* Initialization of values */
-      min_l = 0;
-      min_r = 1;
-      min_sum = arr[0] + arr[1];
-      
-      for(l = 0; l < arr_size - 1; l++)
-      {
-        for(r = l+1; r < arr_size; r++)
-        {
-          sum = arr[l] + arr[r];
-          if(Math.abs(min_sum) > Math.abs(sum))
-          {
-            min_sum = sum;
-            min_l = l;
-            min_r = r;
-          }
-        }
-      }
-      
-      System.out.println(" The two elements whose "+
-                              "sum is minimum are "+
-                        arr[min_l]+ " and "+arr[min_r]);
-    }
-
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in=new Scanner(System.in);
 		List<Integer> ob=new ArrayList<>();
@@ -71,9 +34,6 @@ public class Assignment8 {
 				if(Math.abs(sum)<Math.abs(max) ) {
 					max=sum;
 					pointer=j;
-//					if(max==0) {
-					
-//					}
 				}
 				sum=0;
 				sum+=ob.get(i);
